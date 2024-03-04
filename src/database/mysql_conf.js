33d6daa019dd3bb1245e8e8a70ab7bb74ecc83f4
Jose Sinahy <jslara@proacit.com.mx>
2024-03-04 11:31:14 -0600

    Se resuelve el problema de la conexion

--- src/database/mysql_conf.js
+++ src/database/mysql_conf.js
@@ -10,10 +10,10 @@ const poolConnection = mysql.createPool({
 
 try {
     var connection = mysql.createConnection({
-      host: process.env.HOST_NAME,
-      user: process.env.USER_NAME,
-      password: process.env.PASSWORD,
-      database: process.env.DATABASE_NAME,
+      host: process.env.TICKET_HOST,
+      user: process.env.TICKET_USERNAME,
+      password: process.env.TICKET_PASSWORD,
+      database: process.env.TICKET_DATABASE,
     });
   } catch (error) {
     console.log("Error al conectar con la base de datos");
